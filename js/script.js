49187233f13b244843ac8dc882daa845746a4e06
Mikołaj <mikolaj.lot@gmail.com>
2020-09-12 09:34:04 +0200

    add alt in image and delete lowe code

--- js/script.js
+++ js/script.js
@@ -5,13 +5,6 @@ const listMe = document.querySelector('.list-me')
 const listTechno = document.querySelectorAll('.list-techno')
 const listProject = document.querySelector('.list-project')
 const listContact = document.querySelectorAll('.list-contact')
-const headerHome = document.querySelector('.main__header--home');
-const headerMe = document.querySelector('.main__header--me');
-const headerTechno = document.querySelector('.main__header--techno')
-const headerProject = document.querySelector('.main__header--project')
-const headerContact = document.querySelector('.main__header--contact')
-const scroller = document.querySelector('.scrollTop')
-const header = document.querySelector('.header')
 burger.addEventListener('click', () => {
     burger.classList.toggle('active')
     nav.classList.toggle('active')
